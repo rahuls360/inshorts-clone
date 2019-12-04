583cfa033a3a0819c27193015cc28789b422a2e9
Rahul Makhija <rahuls360@gmail.com>
2019-12-05 00:50:48 +0530

    add icons

--- src/Card.js
+++ src/Card.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {AiTwotoneLike} from 'react-icons/ai';
+import {AiTwotoneDislike} from 'react-icons/ai';
+import {MdFavorite} from 'react-icons/md';
 
 const Card = ({title, description, image, tags}) => {
     return (
@@ -8,6 +11,11 @@ const Card = ({title, description, image, tags}) => {
                 <h3>{title}</h3>
                 <span>{tags.join(' | ')}</span>
                 <p>{description}</p>
+                <div className="interactions">
+                    <AiTwotoneLike color="grey" size="2em"/>
+                    <MdFavorite color="grey" size="2em"/>
+                    <AiTwotoneDislike color="grey" size="2em"/>
+                </div>
             </div>
         </div>
     );
